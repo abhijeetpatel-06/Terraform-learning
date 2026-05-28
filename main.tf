b1d3182189afc85ead750954b26707fd85f42a16
abhijeetpatel-06 <abhijeetpatelsagar@gmail.com>
2026-05-28 10:00:16 +0530

    this is main.tf file changes

--- main.tf
+++ main.tf
@@ -1,1 +1,43 @@
-this is new FIle
+# Configure AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Create Security Group
+resource "aws_security_group" "web_sg" {
+  name        = "web-security-group"
+  description = "Allow SSH and HTTP traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create EC2 Instance
+resource "aws_instance" "web_server" {
+  ami           = "ami-0f58b397bc5c1f2e8"
+  instance_type = "t2.micro"
+
+  security_groups = [aws_security_group.web_sg.name]
+
+  tags = {
+    Name = "Terraform-Web-Server"
+  }
+}
